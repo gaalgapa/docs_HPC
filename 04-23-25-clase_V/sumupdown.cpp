85a1ed4de99b163e6676acc6407f3370d51a8b79
ggarciapa <ggarciapa@unal.edu.co>
2025-04-23 12:11:26 -0500

    comenzando a crear mkaefiles

--- 04-23-25-clase_V/sumupdown.cpp
+++ 04-23-25-clase_V/sumupdown.cpp
@@ -27,7 +27,7 @@ int main(void){
     std::cout.setf(std::ios::scientific);
 
     REAL dir = 0.0, inv = 0.0, diff = 0.0;
-    for(int N = 0; N <= 1e6; N += 50){
+    for(int N = 0; N <= 1e4; N++){
         dir = directo(N);
         inv = inverso(N);
         diff = std::abs(1.0 - dir / inv);
