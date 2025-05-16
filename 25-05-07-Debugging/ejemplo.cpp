39ca48b3144faa90c1943db2a5800921f47542a2
ggarciapa <ggarciapa@unal.edu.co>
2025-05-16 12:05:49 -0500

    actualizado clases pasadas

--- 25-05-07-Debugging/ejemplo.cpp
+++ 25-05-07-Debugging/ejemplo.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
-#include <cstdlib>
 
-int main(int argc, char **argv)
-{
-  // declare variables
-  const int N = 10;
-  double * array;
-  //reserve memory
-  array = new double [N];
-  // initialise array
-  for (int ii = 0; ii < N; ++ii) {
-    array[ii] = 2*(++ii); // != 2*(ii++) ?
-  }
-  // print some values
-  int idx = 2;
-  std::cout << array[idx] << std::endl;
-  idx = 10;
-  //std::cout << array[idx] << std::endl; // compiles, but ...
+int main(void){
+      double x[10], y[5]; // imprime raro
+      //double y[5], x[10]; // imprime 1
 
-  // free memory
-  delete [] array;
+      for(int ii = 0; ii < 5; ++ii) {
+        y[ii] = ii +1;
+      }
+      for(int ii = 0; ii < 10; ++ii) {
+        x[ii] = -(ii +1);
+      }
 
-  return EXIT_SUCCESS;
-}
+      double z = x[10];
+      std::cout << z << std::endl;
+
+      return 0;
+    }
